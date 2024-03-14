9fefda41b876f851ede788e0e9e48120395512aa
hardimahendra <hardimahendra35@gmail.com>
2024-03-14 12:25:23 +0700

    adding swipper effect

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,41 +2,43 @@ import { useEffect, useState } from 'react';
 import { projectData } from '../data/data.json';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Mousewheel, Pagination } from 'swiper/modules';
+import { EffectCube, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-
+import 'swiper/css/effect-cube';
 export default function Project() {
   const projects = projectData;
   const [index, setIndex] = useState(1);
   const [badge, setBadge] = useState(0);
   return (
-    <div className="flex flex-column justify-center itemx-center ">
-      <div className="flex flex-row flex-wrap gap-4 w-full m-3">
-        <div>
-          <img className=" cursor-pointer" width={850} height={300} alt="lathy" src="/images/lathy.png" />
+    <div className=" flex flex-col p-4 gap-4">
+      <div className="flex flex-row flex-wrap gap-4 w-full ">
+        <div className="">
+          <img className="cursor-pointer" width={850} height={300} alt="lathy" src="/images/lathy.png" onClick={() => setIndex(1)} />
         </div>
-        <div className="h-[400px] bg-red-500 ">
+        <div className="h-[200px] items-center pl-10 pt-20">
           <Swiper
-            direction={'vertical'}
-            slidesPerView={1}
-            spaceBetween={30}
-            mousewheel={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Mousewheel, Pagination]}
-            className="mySwiper"
+        effect={'cube'}
+        grabCursor={true}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        pagination={true}
+        modules={[EffectCube, Pagination]}
+        className="mySwiper"
           >
             {projects.map((item) => (
-              <SwiperSlide key={item.id} >
-                <img className="py-1" width={350} height={300} alt="sistem-magang" src={item.image} />
+              <SwiperSlide key={item.id}>
+                <img className="py-1" width={375} height={300} alt="sistem-magang" src={item.image} onClick={() => setIndex(item.id)} />
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
       </div>
-      {/* <div className="">
+      <div className="flex flex-col bg-yellow-500 z-10 w-1/2">
         {projects.map((item) => (
           <div className="" hidden={index != item.id} key={item.id}>
             <h1>{item.title}</h1>
@@ -48,7 +50,7 @@ export default function Project() {
             </div>
           </div>
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
